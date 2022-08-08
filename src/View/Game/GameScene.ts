37584423b56e631ee81740f17292381d06888af5
Alexandr <moyhoroshenkiy@gmail.com>
2022-08-08 20:27:09 +0800

    fixed

--- src/View/Game/GameScene.ts
+++ src/View/Game/GameScene.ts
@@ -2,7 +2,7 @@ import { Composer, Scenes } from "telegraf";
 import { MyContext } from "../../Model/Model";
 require("dotenv").config()
 import * as EmailValidator from 'email-validator';
-import { addDeposit, addEmail, getEmail, getUser } from "../../Controller/UserController";
+import { addDeposit, addEmail, add_coins, getEmail, getUser, lose_coins } from "../../Controller/UserController";
 
 const handler = new Composer<MyContext>();
 const game = new Scenes.WizardScene(
@@ -15,6 +15,7 @@ const game = new Scenes.WizardScene(
 
             if (ctx.update["message"].text == 'Линия поддержки') {
                 const message = "Ты абсолютно прав, это линия поддержки. так как она проходит по нижним точкам графика цены. Лови свои 500 коинов"
+                await add_coins(ctx.from, 500, false)
                 await ctx.replyWithSticker("CAACAgIAAxkBAAINlWLw4TfLvzsDawccQvPswpOo0xO7AAJGBAACP5XMCstXCFgVL57DKQQ")
                 await ctx.reply(message)
             }
@@ -55,7 +56,8 @@ const game = new Scenes.WizardScene(
             // AgACAgIAAxkBAAIKbGLwZn96lKvNYrEG8jjpX9Gxl2k_AALhvzEb-yWAS_bHICEy4NbsAQADAgADcwADKQQ
 
             if (ctx.update["message"].text == 'Ниже') {
-                const message = "Ты абсолютно прав, котировка достигла линии сопротивлени, и скорее всего пошла бы вниз. Лови свои 425 коинов. Мне нравиться твоя ловкость!"
+                const message = "Ты абсолютно прав, котировка достигла линии сопротивлени, и скорее всего пошла бы вниз. Лови свои 455 коинов. Мне нравится твоя ловкость!"
+                await add_coins(ctx.from, 455, false)
                 await ctx.reply(message)
             }
 
@@ -133,6 +135,8 @@ const game = new Scenes.WizardScene(
                 await ctx.reply(message)
                 // @ts-ignore
                 await ctx.replyWithPhoto("AgACAgIAAxkBAAIKbGLwZn96lKvNYrEG8jjpX9Gxl2k_AALhvzEb-yWAS_bHICEy4NbsAQADAgADcwADKQQ", extra)
+
+                ctx.wizard.selectStep(ctx.session.__scenes.cursor + 2)
             }
 
             // AgACAgIAAxkBAAIKbGLwZn96lKvNYrEG8jjpX9Gxl2k_AALhvzEb-yWAS_bHICEy4NbsAQADAgADcwADKQQ
@@ -149,11 +153,210 @@ const game = new Scenes.WizardScene(
                 }
                 // @ts-ignore
                 await ctx.reply(message, extra)
+                ctx.wizard.next()
             }
 
-            ctx.wizard.next()
         }
     },
+
+    async (ctx) => {
+        if (ctx.update["message"]) {
+
+            let extra = {
+                reply_markup: {
+                    keyboard: [['Играть']],
+                    one_time_keyboard: true,
+                    resize_keyboard: true
+                }
+            }
+
+            let message
+
+            if (ctx.update["message"] == "Через 1 час") {
+                message = `Хорошо, напишу через час`
+            }
+            if (ctx.update["message"] == "Через 8 часов") {
+                message = `Хорошо, напишу через 8 часов`
+            }
+            if (ctx.update["message"] == "Через 12 часов") {
+                message = `Хорошо, напишу через 12 часов`
+            }
+            if (ctx.update["message"] == "Через 24 часа") {
+                message = `Хорошо, напишу через сутки`
+            }
+
+            await ctx.replyWithSticker("CAACAgIAAxkBAAINjGLw4FsIsWDIECUejo7RaAvreJElAAI-BAACP5XMCmXGbS9Z4RFmKQQ")
+            await ctx.reply(message, extra)
+        }
+    },
+
+    async (ctx) => {
+        if (ctx.message) {
+
+            const extra = {
+                parse_mode: 'HTML',
+                reply_markup: {
+                    keyboard: [['Конечно', 'Позже']],
+                    one_time_keyboard: true,
+                    resize_keyboard: true
+                }
+            }
+
+            if (ctx.update["message"] == "Выше") {
+                let message = `К сожалению это неверный ответ( Здесь видно, что цена достигла уровня сопротивления, а значит с большей вероятностью она пойдет вниз. Не отчаивайся, у меня еще много интересных заданий!`
+                // @ts-ignore
+                await ctx.reply(message)
+                await ctx.replyWithPhoto("AgACAgIAAxkBAAIKbGLwZn96lKvNYrEG8jjpX9Gxl2k_AALhvzEb-yWAS_bHICEy4NbsAQADAgADcwADKQQ")
+                await ctx.replyWithSticker("CAACAgIAAxkBAAIPbWLw7QKCo0orkR8urtNwcJlXmR69AAJXBAACP5XMCj6R_XixcB-qKQQ")
+                await ctx.reply(`Ты отлично справился с заданиями! И у тебя уже много практики, которую ты можешь перенести на реальную торговлю. Тренируй технический анализ на демо счете, а затем переходи на реальную торговлю \n\nЧтобы проверить баланс введи /profile`)
+                // @ts-ignore
+                await ctx.reply("Как насчет того, чтобы потренировать фундаментальный анализ?", extra)
+                ctx.wizard.next()
+            }
+
+            if (ctx.update["message"] == "Ниже") {
+                let message = `Ты абсолютно прав, котировка достигла линии сопротивления, и скорее всего пошла бы вниз. Лови свои 455 коинов. Мне нравится твоя ловкость!`
+                await add_coins(ctx.from, 455, false)
+                // @ts-ignore
+                await ctx.reply(message)
+                await ctx.replyWithPhoto("AgACAgIAAxkBAAIKbGLwZn96lKvNYrEG8jjpX9Gxl2k_AALhvzEb-yWAS_bHICEy4NbsAQADAgADcwADKQQ")
+                await ctx.replyWithSticker("CAACAgIAAxkBAAIPbWLw7QKCo0orkR8urtNwcJlXmR69AAJXBAACP5XMCj6R_XixcB-qKQQ")
+                await ctx.reply(`Ты отлично справился с заданиями! И у тебя уже много практики, которую ты можешь перенести на реальную торговлю. Тренируй технический анализ на демо счете, а затем переходи на реальную торговлю \n\nЧтобы проверить баланс введи /profile`)
+                // @ts-ignore
+                await ctx.reply("Как насчет того, чтобы потренировать фундаментальный анализ?", extra)
+                ctx.wizard.next()
+            }
+
+        }
+    },
+
+    async (ctx) => {
+        if (ctx.message) {
+            if (ctx.message["text"] == "Конечно") {
+                const extra = {
+                    parse_mode: 'HTML',
+                    reply_markup: {
+                        keyboard: [['Понял, присылай новость']],
+                        one_time_keyboard: true,
+                        resize_keyboard: true
+                    }
+                }
+                ctx.reply("Здорово! Обожаю игры и мне нравиться что ты такой же, как я, хоть и человек, а не бот.")
+                // @ts-ignore
+                ctx.reply("Буду присылать тебе новости которые могут влиять на нашу пару TEA/CFF а ты выбирай, куда пойдет котировка. Но помни, что иногда новость может быть неоднозначной, например одинаково влиять сразу на оба товара в паре. В таких случаях может быть сложно определить направление цены и лучше воздержаться от совершения сделки.", extra)
+                ctx.wizard.next()
+            }
+        }
+    },
+
+    async (ctx) => {
+        if (ctx.message) {
+            if (ctx.message["text"] == 'Понял, присылай новость') {
+
+                const extra = {
+                    parse_mode: 'HTML',
+                    reply_markup: {
+                        keyboard: [['Выше', 'Ниже'], ['Воздержаться от сделки']],
+                        one_time_keyboard: true,
+                        resize_keyboard: true
+                    }
+                }
+
+                // @ts-ignore
+                ctx.reply(`Новость 1. "Ученый Александр Пимштейн сделал невероятное открытие после 5 лет исследований. У 5000 испытуемых женщин, которые пили по 3 чашки кофе в день замедлились процессы старения в 1,5 раза. Он смог открыть невероятную способность кофе к омоложению организма. Похоже даже любители чая по всему миру начинают переходить на кофе". \n\nКак думаешь, куда вероятнее всего пойдет котировка нашего актива TEA/CFF после этой новости? Выбери направление и открой позицию на 500 IQ Coin.`, extra)
+                ctx.wizard.next()
+            }
+        }
+    },
+
+    async (ctx) => {
+        if (ctx.message) {
+
+            const extra = {
+                parse_mode: 'HTML',
+                reply_markup: {
+                    keyboard: [['Понял, присылай следующую новость']],
+                    one_time_keyboard: true,
+                    resize_keyboard: true
+                }
+            }
+
+            let message: string
+
+            if (ctx.message["text"] == "Выше") {
+                message = `К сожалению ты выбрал неверное направление. Если бы наш актив был CFF/TEA то есть кофе был бы базовым товаром, то тогда бы котировка нашего актива пошла вверх, но так как кофе в нашей паре котируемый товар, соответственно его стоимость по отношению к чаю возрастет, то котировка нашего актива после такой новости скорее всего полетит вниз.`
+                await lose_coins(ctx.from, 500, false)
+            }
+
+            if (ctx.message["text"] == 'Ниже') {
+                message = `Ты абсолютно прав! Вероятнее всего из-за большого спроса на кофе стоимость его возрастет, а так как это котируемый товар в нашей паре, то котировка актива скорее всего после этой новости полетит вниз. Лови свои 425 IQ Coin`
+                await add_coins(ctx.from, 455, false)
+            }
+
+            if (ctx.message["text"] == "Воздержаться от сделки") {
+                message = `Вероятнее всего из-за большого спроса на кофе стоимость его возрастет, а так как это котируемый товар в нашей паре, то котировка актива скорее всего после этой новости полетит вниз. Но если ты не уверен, лучше не рисковать. Мне нравиться твоя осторожность!`
+            }
+
+            // @ts-ignore
+            ctx.reply(message, extra)
+            ctx.wizard.next()
+
+        }
+    },
+
+    async (ctx) => {
+        if (ctx.message) {
+            if (ctx.message["text"] == "Понял, присылай следующую новость") {
+                const extra = {
+                    parse_mode: 'HTML',
+                    reply_markup: {
+                        keyboard: [['Понял, присылай следующую новость']],
+                        one_time_keyboard: true,
+                        resize_keyboard: true
+                    }
+                }
+
+                let message = `Лови новость 2. "Самая популярная поп звезда Кэтти Кэндл на свой день рождения устроила большую чайную церемонию. Она сообщила своим гостям, что чай это ее любимый напиток, который помогает ей быть всегда энергичной, и она может давать так много гастролей в том числе благодаря любимому напитку. Похоже фанаты Кэтти Кэндл по всему миру теперь будут пить только чай, позабыв про коллу, кофе и другие напитки"  \n\nКак думаешь, куда вероятнее всего пойдет котировка нашего актива TEA/CFF после этой новости? Выбери направление и открой позицию на 500 IQ Coin.`
+                // @ts-ignore
+                ctx.reply(message, extra)
+                ctx.wizard.next()
+            }
+        }
+    },
+
+    async (ctx) => {
+        if (ctx.message) {
+
+            const extra = {
+                parse_mode: 'HTML',
+                reply_markup: {
+                    keyboard: [['Понял, присылай следующую новость']],
+                    one_time_keyboard: true,
+                    resize_keyboard: true
+                }
+            }
+
+            let message: string
+
+            if (ctx.message["text"] == "Выше") {
+                message = `Ты абсолютно прав! Здесь ситуация изменилась в другую сторону. Так как данная поп звезда является инфлюенсером, который задает тренды ее заявление может повлиять на спрос на чай, а соответственно и на его стоимость. И так как чай в нашей паре базовый товар, то рост его цены, так же повлияет на рост котировки всего актива TEA/CFF. Возможно ты слышал как Илон Маск не раз влиял на стоимость криптовалюты Dogcoin своими твитами. Здесь в нашем игровом мире, похожая ситуация. Лови свои 425 IQ Coin`
+                await add_coins(ctx.from, 455, false)
+            }
+            
+            if (ctx.message["text"] == 'Ниже') {
+                message = `К сожалению ты выбрал неверное направление. Чай в нашей паре базовый актив, а соответственно повышенный спрос на чай повлияет на рост его цены, и котировка актива TEA/CFF будет вероятнее всего рости после этой новости.`
+                await lose_coins(ctx.from, 500, false)
+            }
+
+            if (ctx.message["text"] == "Воздержаться от сделки") {
+                message = `Чай в нашей паре базовый актив, а соответственно повышенный спрос на чай повлияет на рост его цены, и котировка актива TEA/CFF будет вероятнее всего рости после этой новости. Но если ты не уверен, лучше не рисковать. Мне нравиться твоя осторожность!`
+            }
+
+            // @ts-ignore
+            ctx.reply(message, extra)
+
+        }  
+    }
 
 )
 
@@ -175,6 +378,7 @@ game.enter(async (ctx) => {
     ctx.wizard.next()
 })
 
-// game.on("message", ctx => console.log(ctx.message))
+game.on("sticker", ctx => console.log(ctx.message))
+// game.on("sti", ctx => console.log(ctx.message))
 
 export default game
