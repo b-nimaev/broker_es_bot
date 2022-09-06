7f778c3d50addeaf187ab4cd75f6cdf0cf670362
Alexandr <moyhoroshenkiy@gmail.com>
2022-09-06 20:19:40 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,8 @@ import { addDeposit, getEmail } from './Controller/UserController'
 
 // SSL
 const fs = require('fs');
-const key = fs.readFileSync('./ssl/server_rootCA.key');
-const cert = fs.readFileSync('./ssl/server_rootCA.pem');
+const key = fs.readFileSync('./ssl/localhost.key');
+const cert = fs.readFileSync('./ssl/localhost.crt');
 const https = require('https')
 
 const morgan = require("morgan")
@@ -183,8 +183,7 @@ app.use("/interface", interface__);
 // @ts-ignore
 app.get("/", (req: Request, res: Response) => res.send("Hello!"))
 app.use(bot.webhookCallback(secretPath))
-const server = https.createServer({ key, cert }, app);
-server.listen(port, () => console.log("telegram bot launched!"))
+app.listen(port)
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
